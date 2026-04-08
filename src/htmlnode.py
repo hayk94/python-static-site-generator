3512dada1124587d8bad0bb67474dd46a9ea9d2f
Hayk <hayk.safaryan@postcodelotterygroup.com>
2026-04-08 16:22:22 +0200

    add htmlnode

---
+++ src/htmlnode.py
@@ -0,0 +1,24 @@
+class HTMLNode():
+	def __init__(self, tag=None, value=None, children=None, props=None):
+		print(props)
+		self.tag = tag
+		self.value = value 
+		self.children = children 
+		self.props = props 
+
+	def to_html(self):
+		raise NotImplementedError()
+
+	def props_to_html(self):
+		result = ""
+		if self.props is None or len(self.props) <= 0:
+			return result
+
+		for prop in self.props:
+			result += f' {prop}="{self.props[prop]}"'
+
+		return result
+
+	def __repr__(self):
+		print(f"HTMLNode tag: {self.tag} value: {self.value} children: {self.children} props: {self.props}")
+		
